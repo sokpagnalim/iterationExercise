91a91c1f17039efd1cd78c718f5cdae0d2025115
Sokpagna Lim <sokpagnalim@gmail.com>
2022-06-05 21:41:15 -0700

    IterationExercise

---
+++ IterationExercise/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace IterationExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            { 
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 10; i++)
+            { 
+            Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+        }
+    }
+}
